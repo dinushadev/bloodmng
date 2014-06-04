723fdb1fe53aed6ef925d412f2fbfbe8e601953f
dinusha <dinusha.weerasekara@gmail.com>
2014-06-05 00:24:28 +0530

    add db cofig login servis

--- app.js
+++ app.js
@@ -28,30 +28,46 @@ app.engine('html', ejs.renderFile);
 /**
 * Render the login page.
 */
-app.get('/',  function login(request, response) {
+app.get('/',  function index(request, response) {
 
 	response.render('index.html');
 });
 
-app.get('/login',  function login(request, response) {
+app.post('/login',  function login(request, response) {
 
-	console.log('ddd'+config.dburl);	
+	//console.log('psurl'+config.dburl);	
+	var usrname=request.body.username;
+	var pass =request.body.password;
 
 	pg.connect(config.dburl, function(err, client) {
 		if(err){
 			console.log('Error in connect to db: '+err);
 		}
-  var query = client.query('SELECT * FROM users;');
+		//var query = client.query("SELECT * FROM users WHERE uname=dinu");
+		     var query = client.query("SELECT uname,pass,email FROM users WHERE uname = $1", [usrname]);
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
+		query.on('row', function(row) {
+			console.log('row:'+JSON.stringify(row));
+			if(row){
+				if(row.pass == pass){
+					//TODO add session data
+					response.redirect('/home');
+					//return;
+				}else{
+					//invalid pass
+					response.redirect('/');
+				}
+			}else{
+				//invalid usrname
+				response.redirect('/');
+			}
+		});
+	});
+
+	
 });
 
-	response.render('index.html');
-});
-
-app.get('/home',  function login(request, response) {
+app.get('/home',  function home(request, response) {
 
 	response.render('home.html');
 });
@@ -67,7 +83,7 @@ csrfToken: request.session._csrf
 
 
 app.listen(app.get('port'), function() {
-console.log("Node app is running at localhost:" + app.get('port'));
+	console.log("Node app is running at localhost:" + app.get('port'));
 });
 
 
