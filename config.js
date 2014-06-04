dfb8d91a3f9870bcf3630f709fc8b3b8aad3e21c
dinusha <dinusha.weerasekara@gmail.com>
2014-06-05 00:27:40 +0530

    change db config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	/*dev: {
+	dev: {
 		dburl: 'postgres://zkptrngworkzwk:Hriw4ALv-RyIJ3ATnpCMZM2QVu@ec2-54-225-182-133.compute-1.amazonaws.com:5432/d2medijeln2fpc?sslmode=require'
 		
-    }*/
+    }
 
-    dev: {
+   /* dev: {
 		dburl: 'postgres://dbuser1:dbuser@localhost:5432/bloodmg'
-		
+		*/
     }
 }
